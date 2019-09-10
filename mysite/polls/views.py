0ed39b69e7332dc95f23069762ea9fedf66d2aef
kornkritpawit <konsoo@hotmail.com>
2019-09-10 15:48:03 +0700

    part 5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.views import generic
 
 from .models import Choice, Question
+from django.utils import timezone
 
 
 class IndexView(generic.ListView):
@@ -18,6 +19,13 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 
 class ResultsView(generic.DetailView):
@@ -41,4 +49,13 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        
+    def get_queryset(self):
+        """
+    Return the last five published questions (not including those set to be
+    published in the future).
+        """
+        return Question.objects.filter(
+        pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+    
     
