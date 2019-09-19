c7c1b26f9ba7aee8c06a0f678314fe519225f407
kornkritpawit <konsoo@hotmail.com>
2019-09-19 10:55:57 +0700

    change location

--- polls/models.py
+++ polls/models.py
@@ -10,6 +10,13 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def vote_count(id):
+        vote = 0
+        for choice in Question.objects.get(id=id).choice_set.all():
+            vote += choice.votes
+        return vote 
+    
         
 
     def __str__(self):
@@ -21,6 +28,11 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def find_polls_for_text(text):
+        quest = Question.objects.filter(question_text__contains=text)
+        return quest
     
     def __str__(self):
         return self.choice_text
+    
